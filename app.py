d2fbbc24aa91379284cf5037223446b1287084e3
MaxJoas <maxjoas.mj@gmail.com>
2021-02-24 15:43:32 +0100

    reduce slug size

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from werkzeug.utils import secure_filename
+
+import cv2
 import numpy as np
-from flask import Flask, flash, request, redirect, render_template, url_for, send_from_directory
+from flask import (Flask, flash, request, redirect, render_template, url_for,
+                   send_from_directory)
 import urllib.request
 import os
 from flask import Flask
 from keras.preprocessing import image
-
+from keras.applications.resnet50 import (ResNet50, decode_predictions,
+                                         preprocess_input)
 import keras
 app = Flask(__name__)
 
@@ -16,6 +20,11 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 IMG_FOLDER = '/tmp/'
 
 app.config['IMG_FOLDER'] = IMG_FOLDER
+
+# Global variables for Image classification
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier(
+    'haarcascades/haarcascade_frontalface_alt.xml')
 
 
 def _get_img_path(image):
@@ -194,16 +203,74 @@ def Xception_predict_breed(img_path):
     return dog_names[np.argmax(predicted_vector)]
 
 
+ResNet50_model = ResNet50(weights='imagenet')
+
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+
+# returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151))
+
+
+def classify_dog(path):
+    """Classifies a given image wheter it is a dog or human and returns breed of the dog
+
+    Args:
+        path - (str): path to the image that should be classified
+
+    Returns:
+        dog_breed - (str): Name of the classified dog bred
+    """
+
+    # check what kind of image is given
+    is_dog = dog_detector(path)
+    is_human = face_detector(path)
+
+    # breaks if image is neither dog or human
+    if not is_dog and not is_human:
+        message = "The image could not be identified as dog or human face. \n \
+            Please provide a valid image"
+        return message
+
+    # detect dog breed
+    if is_dog:
+        breed = Xception_predict_breed(path)
+        # modfiy breed name to be more readable
+        message = "Your dog's breed is {}".format(breed)
+        return message
+
+    if is_human:
+        breed = Xception_predict_breed(path)
+        message = "This looks more like a human! \n Anyway, let's see which \
+            dog breed looks similar to this human. \n This human looks \
+            like a {}".format(breed)
+        return message
+
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
+@ app.route('/')
 def upload_form():
     return render_template('upload.html')
 
 
-@app.route('/', methods=['POST'])
+@ app.route('/', methods=['POST'])
 def upload_file():
     if not os.path.exists('/tmp/'):
         directory = "tmp"
@@ -223,11 +290,11 @@ def upload_file():
             filename = secure_filename(file.filename)
             load_model()
             file.save(_get_img_path(filename))
-            preds = Xception_predict_breed(_get_img_path(filename))
-            print(preds)
+            # preds = Xception_predict_breed(_get_img_path(filename))
+            preds = classify_dog(_get_img_path(filename))
             # return send_from_directory(app.config['IMG_FOLDER'], filename)
             # return redirect(url_for('display_image', filename=filename))
-            flash('Thist dog is a {}'.format(preds))
+            flash(preds)
             # return redirect('/')
             return render_template('upload.html', filename=filename)
 
