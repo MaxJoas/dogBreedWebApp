e3728f62d20311e05f33ed915e6fc5b6406e901f
MaxJoas <maxjoas.mj@gmail.com>
2021-02-24 10:10:57 +0100

    temp folder

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.secret_key = "secret key"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-IMG_FOLDER = 'static/img'
+IMG_FOLDER = '/temp'
 
 app.config['IMG_FOLDER'] = IMG_FOLDER
 
